4fed8585d89469529888e9d6cd835adb61e8b814
baraneetharan <baraneetharan@live.com>
2023-12-29 17:23:06 +0530

    Add files via upload

---
+++ src/main/java/com/kgisl/sportsacademy/SportsacademyApplication.java
@@ -0,0 +1,13 @@
+package com.kgisl.sportsacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsacademyApplication.class, args);
+	}
+
+}
